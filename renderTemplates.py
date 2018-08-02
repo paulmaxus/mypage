d924d7a55c5ea07f9a2364bfe4f94781b083a534
paulmaxus <paulmaxus@gmail.com>
2018-08-02 10:41:38 +0200

    update version2

--- renderTemplates.py
+++ renderTemplates.py
@@ -17,12 +17,42 @@ env = Environment(
 def render_template(filename, context):
     return env.get_template(filename).render(context)
 
-pages = ['intro','research']
-projects = ['shiny','nlp']
+# variables to pass to context
+pages = ['intro','research','events']
+projects = ['nlp','shiny']
+events = [
+          [2018,
+            [
+              ['09','NE, Den Haag','Project meeting'
+              ],
+              ['07','NE, University Medical Center Groningen', 
+              'Summer school on neural speech processing',
+              '<a target="_blank" href="docs/maps.pdf">poster</a>'
+              ],
+              ['04','UK, Leicester','Experimental Psychology Society meeting',
+              '<a target="_blank" href="docs/eps.pdf">poster</a>'
+              ],
+              ['03','NE, Nijmegen','Project meeting',
+              '<a target="_blank" href="docs/nijmegen.pdf">presentation</a>'
+              ]
+            ]
+          ],
+          [2017,
+            [
+              ['10','ES, Vitoria','Project meeting and Python training'],
+              ['07','EL, Heraklion','<a href="#char">Software project</a>, entrepreneurship in hearing technology, summer school on speech synthesis'
+              ],
+              ['04','UK, Edinburgh','Workshop on cognitive effort'
+              ]
+            ]
+          ]
+         ]
+
 context = {
     'pages' : pages,
     'projects' : projects,
-    'thisPage' : ''
+    'thisPage' : '',
+    'events' : events
 }
 
 outPath = 'mypage/website/'
